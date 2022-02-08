8b16350e78a1ecf78d706ca9eb619c49c7abb942
robo-monk <rrobomonk@gmail.com>
2022-02-08 23:53:26 +0200

    init from file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
 use std::collections::HashMap;
 use std::io::Error;
+use std::io::Read;
+use std::str::FromStr;
+
+const DB_FILE: &str = "db.txt";
 
 struct Todo {
     // use rust built in HashMap to store key - val pairs
     map: HashMap<String, bool>,
 }
 
+
 impl Todo {
+
+    fn new() -> Result<Todo, Error> {
+        let mut f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open(DB_FILE)?;
+
+        let mut content = String::new();
+        f.read_to_string(&mut content)?;
+
+        let mut map = HashMap::new();
+
+        for entries in content.lines() {
+            let mut values = entries.split("\t");
+            let key = values.next().expect("No key");
+            let val = values.next().expect("No value");
+            map.insert(String::from(key), bool::from_str(val).unwrap());
+        }
+
+        Ok(Todo { map })
+    }
+
     fn insert(&mut self, key: String) {
         self.map.insert(key, true);
     }
@@ -21,7 +49,7 @@ impl Todo {
             content.push_str(&record);
         }
 
-        std::fs::write("db.txt", content)
+        std::fs::write(DB_FILE, content)
     }
 }
 
@@ -31,9 +59,12 @@ fn main() {
     let item = std::env::args().nth(2).expect("Please specify an item");
     println!("{:?}, {:?}", action, item);
 
-    let mut todo = Todo {
-        map: HashMap::new(),
-    };
+    //let mut todo = Todo {
+        //map: HashMap::new(),
+    //};
+    //
+    let mut todo = Todo::new().expect("Initialisation of db failed");
+
     
     if action == "add" {
         todo.insert(item);
@@ -43,3 +74,4 @@ fn main() {
         }
     }
 }
+
