432e752ee77769a059d22a21684f12fe8a4ab944
robo-monk <rrobomonk@gmail.com>
2022-02-09 02:01:00 +0200

    rust is sick

--- src/main.rs
+++ src/main.rs
@@ -23,20 +23,47 @@ impl Todo {
         let mut content = String::new();
         f.read_to_string(&mut content)?;
 
-        let mut map = HashMap::new();
-
-        for entries in content.lines() {
-            let mut values = entries.split("\t");
-            let key = values.next().expect("No key");
-            let val = values.next().expect("No value");
-            map.insert(String::from(key), bool::from_str(val).unwrap());
-        }
+        let map: HashMap<String, bool> = content
+            .lines()
+            .map(|line| line.splitn(2, "\t").collect::<Vec<&str>>())
+            .map(|ary| (ary[0], ary[1]))
+            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
+            .collect();
 
         Ok(Todo { map })
     }
 
     fn insert(&mut self, key: String) {
-        self.map.insert(key, true);
+        self.map.insert(key, false);
+    }
+
+    fn set(&mut self, key: String, value: bool) {
+        match self.map.get_mut(&key) {
+          Some(v) => Some(*v = value),
+          None => None,
+        };
+    }
+
+    fn complete(&mut self, key: String) {
+        self.set(key, true);
+    }
+
+    fn incomplete(&mut self, key: String) {
+        self.set(key, false);
+    }
+
+    fn delete(&mut self, key: String) {
+        self.map.remove(&key);
+    }
+
+    fn pretty_list(&mut self) -> String {
+        let mut list = String::new();
+        for (task, done) in &self.map {
+            let checkmark = if *done { "✓" } else { " " };
+            let record = format!("[{}] {}\n", checkmark, task);
+            list.push_str(&record);
+        }
+        list
     }
 
     fn save(self) -> Result<(), Error> {
@@ -51,27 +78,63 @@ impl Todo {
 
         std::fs::write(DB_FILE, content)
     }
+
+}
+
+#[derive(Debug)]
+enum Action {
+    Add, 
+    Done,
+    Undone,
+    Delete,
+    List,
 }
 
 fn main() {
     //println!("Hello, world!");
-    let action = std::env::args().nth(1).expect("Please specify an action");
-    let item = std::env::args().nth(2).expect("Please specify an item");
-    println!("{:?}, {:?}", action, item);
+    let params: Vec<String> = std::env::args().collect();
+    let command = &params[1];
+    let mut argument: String = params[2..].join(" "); 
 
-    //let mut todo = Todo {
-        //map: HashMap::new(),
-    //};
-    //
-    let mut todo = Todo::new().expect("Initialisation of db failed");
+    //let action = std::env::args().nth(1).expect("Please specify an action");
+            //.map(|p| println!("{}", p));
+    //println!("{:?}", &params[1..]);
 
     
-    if action == "add" {
-        todo.insert(item);
-        match todo.save() {
-            Ok(_) => println!("todo saved!"), 
-            Err(why) => println!("bruh {}", why), 
-        }
+
+    //let actions = Actions {
+         //add: String::from("add")
+    //};
+
+    let mut todo = Todo::new().expect("Initialisation of db failed");
+
+    let action = match command.as_str() {
+        "a" | "add" | "insert" | "touch" => Action::Add,
+        "r" | "rem" | "rm" | "del" | "delete" | "destroy" => Action::Delete,
+        "d" | "do" | "done" | "complete"  => Action::Done,
+        "u" | "undo" | "undone"  => Action::Undone,
+        "v" | "view" | "show" | "ls" | "list" => Action::List,
+        _ => {
+            argument = format!("{} {}", command, argument);
+            Action::Add
+        },
+    };
+
+
+    // do whatever in the todolist
+    match action {
+        Action::Add => todo.insert(argument),
+        Action::Done => todo.complete(argument),
+        Action::Undone => todo.incomplete(argument),
+        Action::Delete => todo.delete(argument),
+        Action::List => print!("{}", todo.pretty_list()),
+    }
+
+    //println!("{:#?}", action);
+
+    match todo.save() {
+        Ok(_) => println!("-- rastadone v 420--"), 
+        Err(why) => println!("bruh {}", why), 
     }
 }
 
